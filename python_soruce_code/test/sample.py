c53861cd9d6b6d186d80560eed92e38fb88b1a70
Saul <sauld@cimat.mx>
2018-10-10 08:39:47 -0700

    Deterministic simulation model. Figure implementation.

--- python_soruce_code/test/sample.py
+++ python_soruce_code/test/sample.py
@@ -3,18 +3,21 @@ from fbm import FBM
 import matplotlib.pyplot as plt
 import time
 import math
+import numpy as np
 
 
-def h(t):
+def h(s):
     # return 0.499*math.sin(t) + 0.5
     # return 0.6 * t + 0.3
-    return 0.5 * math.exp(-8*t**2) + 0.35
+    return 0.5 * np.exp(-8.0 * s ** 2)
 
 
-fbm_generator = FBM(2 ** 8, .88)
+fbm_generator = FBM(2 ** 8, 0.85)
 t = fbm_generator.times()
 fbm_realization = fbm_generator.fbm()
 fgn_realization = fbm_generator.fgn()
+h_t = np.array(h(fbm_realization))
 plt.plot(t, fbm_realization)
-plt.plot(t[0:-1], fgn_realization)
+plt.plot(t, h_t)
+# plt.plot(t[0:-1], fgn_realization)
 plt.show()
